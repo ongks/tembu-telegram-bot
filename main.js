7049b974d8ead5f692a8f0725d51c9573b93d8a7
SoraSkyy <viorama@gmail.com>
2017-07-20 22:45:40 +0800

    Added gitignore for emacs and fixed syntax issues for main.js.

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 import TelegramBot from 'node-telegram-bot-api';
+import StateManager from './statemanager';
 import { credentials } from './config';
 
 const bot = new TelegramBot(credentials.token, { polling: true, });
 
+const stateManagers = {};
 
-bot.on('message', (msg, cb) => {
+bot.on('message', (msg, calback) => {
+  if (!stateManagers[msg.chat.id]) {
+    stateManagers[msg.chat.id] = new StateManager(msg.chat.id);
+  }
+  const response = stateManagers[msg.chat.id].process(msg);
+  if (response.respond) {
+    bot.sendMessage(response.chatID, response.text, response.options || {});
+  }
   console.log(msg);
 });
+
+
+
+
+
+
+
 
 
 
